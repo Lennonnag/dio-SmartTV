f6ecea1af0eccfa1dc2764b0a58f6c1c5d82cc12
Lennonnag <lennon_nagata@hotmail.com>
2023-03-01 21:57:12 -0300

    adicionado comandos da tv

--- src/edu/lennon/smart/SmartTv.java
+++ src/edu/lennon/smart/SmartTv.java
@@ -1,5 +1,7 @@
 package edu.lennon.smart;
 
+import java.security.Principal;
+
 public class SmartTv {
     boolean ligada = false;
     int canal = 1;
@@ -7,8 +9,31 @@ public class SmartTv {
 
     public void ligar() {
         ligada = true;
+        System.out.println("Ligando");
     }
     public void desligar(){
         ligada = false;
+        System.out.println("Desligando");
+
+    }
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Vol+" + volume);
+    }
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Vol-" + volume);
+    }
+    public void aumentarCanal() {
+        canal++;
+        System.out.println("Canal+" + canal);
+    }
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("Canal-" + canal);
+    }
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+        System.out.println("Mudando canal para: " + canal);
     }
 }
--- src/edu/lennon/smart/Usuario.java
+++ src/edu/lennon/smart/Usuario.java
@@ -3,14 +3,22 @@ package edu.lennon.smart;
 public class Usuario {
     public static void main(String[] args) {
         SmartTv smartTv = new SmartTv();
-        //String estado = "Desligada";
-        
+        // String estado = "Desligada";
 
         System.out.println("Tv ligada: " + smartTv.ligada);
         System.out.println("Volume atual: " + smartTv.volume);
         System.out.println("Canal atual: " + smartTv.canal);
         smartTv.ligar();
-       
-        System.out.println("Tv ligada: " + estado);
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.mudarCanal(13);
+        System.out.println("Tv ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+        smartTv.desligar();
     }
 }
